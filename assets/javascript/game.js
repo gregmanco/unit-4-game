5bb671bde4bf9fd9cd8de9e316f6f05c59c2ba71
Greg Manco <gregmanco@gmail.com>
2019-06-30 18:11:33 -0400

    Added Pseudo code to game.js and basic heading / body to index.html

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+// Game Variables 
+
+// 4 Foods each with a random number that will reset on win or loss condition
+
+// New Computer input that will be random and reset on win or loss condition
+
+// Wins
+
+// Losses
+
+
+// When clicking a food item it should add its sum to the total score.
+
+// If total score = Computer Input then increase wins+ and reset
+
+// If total score > Computer Input than increase loss and reset
+
+//
